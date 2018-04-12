14e6448fae0f22e1f0872cb6be2375ae94ae9953
Wataru Maeda <w.maeda@neogenia.co.jp>
2018-04-13 02:15:59 +0900

    feat: ブラックボックステストの仕組みを実装

--- backend-svc/docker/resources/docker-entrypoint.sh
+++ backend-svc/docker/resources/docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 /etc/init.d/ssh start
 
+if [ -n "$TEST" ]; then
+  exec bash -c 'ROUTE=$TEST ruby /mnt/blackbox_test/test_v1.rb'
+fi
+
 bash /mnt/xml_rpc_server/start_server.sh $RPC_SERVER_PORT
 
 echo '----- STARTED -----'
