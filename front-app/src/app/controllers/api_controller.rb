f9fc15072c7edb69e6941bcbd82d2bb73f38884f
Wataru Maeda <w.maeda@neogenia.co.jp>
2018-04-18 20:34:17 +0900

    feat: フロントの詳細編集画面を実装。CSSも頑張った。

--- front-app/src/app/controllers/api_controller.rb
+++ front-app/src/app/controllers/api_controller.rb
@@ -38,7 +38,49 @@ class ApiController < ApplicationController
     render json: result
   end
 
+  def detail
+    input_data = {
+      command: "detail",
+      options: {
+        id: params[:id]
+      }
+    }
+
+    result = _call_backend input_data
+    #result = {
+    #  output: {
+    #    id: 1,
+    #    title: 'テスト',
+    #    notify_datetime: '2018-03-29T19:32:00+0900',
+    #    term: '2018-03-29T19:32:00+0900',
+    #    created_at: '2018-03-29T19:32:00+0900',
+    #    finished_at: '2018-03-29T19:32:00+0900',
+    #    memo: 'めもめお'
+    #  }.to_json
+    #}
+    render json: result
+  end
+
   def edit
+    input_data = {
+      command: "edit",
+      options: {
+        id: params[:id],
+        title: params[:title],
+        notify_datetime: params[:notify_datetime],
+        term: params[:term],
+        memo: params[:memo]
+      }
+    }
+
+    result = _call_backend input_data
+    #result = {
+    #  output: {
+    #    status: 'ok',
+    #    created_at: '2018-03-29T19:32:00+0900',
+    #  }.to_json
+    #}
+    render json: result
   end
 
   def finish
