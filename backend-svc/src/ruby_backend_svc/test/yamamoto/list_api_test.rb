2c669c155e9d77374fa1610c09af4c5320d8028d
TomoProg <t.yamamoto@neogenia.co.jp>
2018-04-25 21:16:47 +0900

    test: ListApiのテストを修正およびunfinishedのときのテストを追加

--- backend-svc/src/ruby_backend_svc/test/yamamoto/list_api_test.rb
+++ backend-svc/src/ruby_backend_svc/test/yamamoto/list_api_test.rb
@@ -1,20 +1,31 @@
 require_relative './base_test'
+require 'pry'
 
 module Yamamoto
   class ListApiTest < BaseTest
     def setup
       super
-      create_test_data('1.json', '{"title":"xxxxxx","term":"2018-03-20T19:32:00+0900"}')
-      create_test_data('2.json', '{"title":"yyyyyy","term":"2018-03-20T19:32:00+0900"}')
-      create_test_data('3.json', '{"title":"zzzzzz","term":"2018-03-21T19:32:00+0900"}')
+      create_test_data('1.json', '{"title":"xxxxxx","term":"2018-03-20T19:32:00+09:00","finished_at":"2018-03-22T19:32:00+09:00"}')
+      create_test_data('2.json', '{"title":"yyyyyy","term":"2018-03-20T19:32:00+09:00"}')
+      create_test_data('3.json', '{"title":"zzzzzz","term":"2018-03-21T19:32:00+09:00"}')
     end
 
 
-    def test_全件取得
-      result = ListApi.new.run(JSON.parse('{"options":{"condition":"all"}}'))
-      assert_equal result[:list][0], { 'title' => 'xxxxxx', 'term' => '2018-03-20T19:32:00+0900', 'id' => 1 }
-      assert_equal result[:list][1], { 'title' => 'yyyyyy', 'term' => '2018-03-20T19:32:00+0900', 'id' => 2 }
-      assert_equal result[:list][2], { 'title' => 'zzzzzz', 'term' => '2018-03-21T19:32:00+0900', 'id' => 3 }
+    def test_all
+      json_data = {'options' => {'condition' => 'all'}}
+      result = ListApi.new.run(json_data)
+      assert_equal 1, result[:list][0]['id']
+      assert_equal 'xxxxxx', result[:list][0]['title']
+      assert_equal '2018-03-20T19:32:00+09:00', result[:list][0]['term']
+      assert_equal true, result[:list][0]['finished']
+      assert_equal 2, result[:list][1]['id']
+      assert_equal 'yyyyyy', result[:list][1]['title']
+      assert_equal '2018-03-20T19:32:00+09:00', result[:list][1]['term']
+      assert_equal false, result[:list][1]['finished']
+      assert_equal 3, result[:list][2]['id']
+      assert_equal 'zzzzzz', result[:list][2]['title']
+      assert_equal '2018-03-21T19:32:00+09:00', result[:list][2]['term']
+      assert_equal false, result[:list][2]['finished']
     end
 
     def test_本日のみ取得
@@ -25,6 +36,19 @@ module Yamamoto
       #        { id: 3, title: "zzzzzz", term: "2018-03-21T19:32:00+0900" },
       #    ]
       #}.to_json
+    end
+
+    def test_unfinished
+      json_data = {'options' => {'condition' => 'unfinished'}}
+      result = ListApi.new.run(json_data)
+      assert_equal 2, result[:list][0]['id']
+      assert_equal 'yyyyyy', result[:list][0]['title']
+      assert_equal '2018-03-20T19:32:00+09:00', result[:list][0]['term']
+      assert_equal false, result[:list][0]['finished']
+      assert_equal 3, result[:list][1]['id']
+      assert_equal 'zzzzzz', result[:list][1]['title']
+      assert_equal '2018-03-21T19:32:00+09:00', result[:list][1]['term']
+      assert_equal false, result[:list][1]['finished']
     end
   end
 end
