9df24aef3e980a0a47d17fc0607e5b70f56b8569
TomoProg <t.yamamoto@neogenia.co.jp>
2018-04-25 20:50:02 +0900

    fix: data_accessor_test修正
    ついでにupdateとdeleteのテスト追加

--- backend-svc/src/ruby_backend_svc/test/yamamoto/data_accessor_test.rb
+++ backend-svc/src/ruby_backend_svc/test/yamamoto/data_accessor_test.rb
@@ -5,8 +5,8 @@ module Yamamoto
     def setup
       # ファイル作成
       super
-      create_test_data('1.json', '{"title":"xxxxxx","term":"2018-03-20T19:32:00+0900"}')
-      create_test_data('2.json', '{"title":"yyyyyy","term":"2018-03-20T19:32:00+0900"}')
+      create_test_data('1.json', '{"title":"xxxxxx","term":"2018-03-20T19:32:00+09:00"}')
+      create_test_data('2.json', '{"title":"yyyyyy","term":"2018-03-20T19:30:00+09:00"}')
     end
 
     def teardown
@@ -15,21 +15,55 @@ module Yamamoto
 
     def test_read
       data = @data_accessor.read(1)
-      assert_equal({'title' => 'xxxxxx', 'term' => '2018-03-20T19:32:00+0900', 'id' => 1}, data)
+      assert_equal 1, data['id']
+      assert_equal 'xxxxxx', data['title']
+      assert_equal '2018-03-20T19:32:00+09:00', data['term']
     end
 
     def test_all_read
       data = @data_accessor.all_read
       assert_equal 2, data.length
-      assert_equal({'title' => 'xxxxxx', 'term' => '2018-03-20T19:32:00+0900', 'id' => 1}, data[0])
-      assert_equal({'title' => 'yyyyyy', 'term' => '2018-03-20T19:32:00+0900', 'id' => 2}, data[1])
+      assert_equal 1, data[0]['id']
+      assert_equal 'xxxxxx', data[0]['title']
+      assert_equal '2018-03-20T19:32:00+09:00', data[0]['term']
+      assert_equal 2, data[1]['id']
+      assert_equal 'yyyyyy', data[1]['title']
+      assert_equal '2018-03-20T19:30:00+09:00', data[1]['term']
     end
 
     def test_create
-      data = @data_accessor.create({'title' => 'zzzzzz','term' => '2018-03-20T19:32:00+0900'})
-      assert data > 0
-      assert File.exist?(File.join(@data_path, '3.json'))
-      assert_equal '{"title":"zzzzzz","term":"2018-03-20T19:32:00+0900"}', File.read(File.join(@data_path, '3.json'))
+      data = @data_accessor.create({'title' => 'zzzzzz','term' => '2018-03-20T20:32:00+09:00'})
+      assert_equal 3, data['id']
+      assert_equal 'zzzzzz', data['title']
+      assert_equal '2018-03-20T20:32:00+09:00', data['term']
+      assert data['created_at'].length > 0
+
+      # 実際のファイルの中身も確認
+      data = JSON.parse(File.read(File.join(@data_path, '3.json')))
+      assert_equal 'zzzzzz', data['title']
+      assert_equal '2018-03-20T20:32:00+09:00', data['term']
+      assert data['created_at'].length > 0
+    end
+
+    def test_update
+      data = @data_accessor.update({'title' => 'zzzzzz', 'aaa' => 'bbb'}, 1)
+      assert_equal 1, data['id']
+      assert_equal 'zzzzzz', data['title']
+      assert_equal '2018-03-20T19:32:00+09:00', data['term']
+      assert_equal 'bbb', data['aaa']
+      assert data['updated_at'].length > 0
+
+      # 実際のファイルの中身も確認
+      data = JSON.parse(File.read(File.join(@data_path, '1.json')))
+      assert_equal 'zzzzzz', data['title']
+      assert_equal '2018-03-20T19:32:00+09:00', data['term']
+      assert_equal 'bbb', data['aaa']
+      assert data['updated_at'].length > 0
+    end
+
+    def test_delete
+      @data_accessor.delete(1)
+      refute File.exist?(File.join(@data_path, '1.json'))
     end
 
     def test_next_id
