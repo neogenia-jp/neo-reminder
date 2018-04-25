15d698a5dc7085b9f3794b05c4e2d9ecacbb88e4
TomoProg <t.yamamoto@neogenia.co.jp>
2018-04-25 21:26:08 +0900

    feat: 登録APIの戻り値に作成日を追加

--- backend-svc/src/ruby_backend_svc/src/yamamoto/create_api.rb
+++ backend-svc/src/ruby_backend_svc/src/yamamoto/create_api.rb
@@ -9,12 +9,14 @@ module Yamamoto
       result = {}
 
       begin
-        @data_accessor.create(json_data['options'])
+        data = @data_accessor.create(json_data['options'])
         result[:status] = 'ok'
         result[:message] = ''
+        result[:created_at] = data['created_at']
       rescue => e
         result[:status] = 'error'
         result[:message] = e.message
+        result[:created_at] = ''
       end
 
       result
