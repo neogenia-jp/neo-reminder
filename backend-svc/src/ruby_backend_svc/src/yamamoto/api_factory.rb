c21543c3307343121ab283e98c5b409d5aab18d7
TomoProg <t.yamamoto@neogenia.co.jp>
2018-05-12 22:07:39 +0900

    refactor: Apiを追加したときにFactoryに書かなくて済むように変更

--- backend-svc/src/ruby_backend_svc/src/yamamoto/api_factory.rb
+++ backend-svc/src/ruby_backend_svc/src/yamamoto/api_factory.rb
@@ -1,36 +1,8 @@
-require_relative './list_api'
-require_relative './create_api'
-require_relative './delete_api'
-require_relative './detail_api'
-require_relative './edit_api'
-require_relative './finish_api'
-require_relative './clear_api'
-require_relative './observe_api'
-
 module Yamamoto
   class ApiFactory
     def self.get(command_name)
-      # TODO いちいちAPIに応じて書くのめんどい
-      case command_name
-        when 'list'
-          ListApi.new
-        when 'create'
-          CreateApi.new
-        when 'delete'
-          DeleteApi.new
-        when 'detail'
-          DetailApi.new
-        when 'edit'
-          EditApi.new
-        when 'finish'
-          FinishApi.new
-        when 'clear'
-          ClearApi.new
-        when 'observe'
-          ObserveApi.new
-        else
-          nil
-      end
+      require_relative "./#{command_name}_api"
+      Yamamoto.const_get("#{command_name.capitalize}Api").new
     end
   end
 end
