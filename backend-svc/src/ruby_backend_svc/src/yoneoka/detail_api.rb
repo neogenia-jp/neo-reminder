083f98dc48b199ea5f24696b2d2d5a68154a4550
t.yoneoka <t.yoneoka@neogenia.co.jp>
2018-05-16 21:10:37 +0900

    api v2を実装(observe,clear)

---
+++ backend-svc/src/ruby_backend_svc/src/yoneoka/detail_api.rb
@@ -0,0 +1,15 @@
+require_relative './base_api'
+
+module Yoneoka
+  class DetailApi < BaseApi
+
+    # 実行
+    # @param json_data [Hash] 送られてきたJSONデータ
+    # @return [Hash]
+    def run(json_data)
+      # リマインダーを取得する
+      @data_accessor.read(json_data['options']['id'])
+    end
+  end
+end
+
