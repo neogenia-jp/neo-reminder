59601c531e2140ceee8ddf0a90cab6a41c1664dc
foo <bar>
2018-05-29 23:51:00 +0900

    fix: 一覧取得APIに条件unfinishedを追加

--- backend-svc/src/ruby_backend_svc/src/kamada/list_api.rb
+++ backend-svc/src/ruby_backend_svc/src/kamada/list_api.rb
@@ -9,23 +9,44 @@ module Kamada
     # @return [Hash]
     def run(json_data)
 
-      # リマインダーを取得する
-      day = json_data['options'] == 'today' ? Date.today : nil
+      case json_data['options']
+      when 'all'
+        list = reminder_list_all
+      when 'today'
+        list = reminder_list_today
+      when 'unfinished'
+        list = reminder_list_unfinished
+
+      # 完了しているかどうかのフラグを付与する
+      list.each do |r|
+        r['finished'] = r['finished_at'].nil? ? false : true
+      end
 
       # JSON形式にフォーマットし文字列で返す
-      { list: reminder_list(day) }
+      { list: list }
     end
 
     private
 
-    # リマインダーを取得する
-    # @param day [Date] 日付
-    # @return [String]
-    def reminder_list(day)
+    # リマインダーを取得する（全て）
+    # @return [Array]
+    def reminder_list_all
+      @data_accessor.all_read
+    end
+
+    # リマインダーを取得する（本日）
+    # @return [Array]
+    def reminder_list_today
+      day = Date.today
       data = @data_accessor.all_read
-      return data if day.nil?
       data.select {|d| Date.parse(d['notify_datetime']) == day}
+    end
+
+    # リマインダーを取得する（未完了のみ）
+    # @return [Array]
+    def reminder_list
+      data = @data_accessor.all_read
+      data.select {|d| d['finished_at'].nil?}
     end
   end
 end
-
