f0e7ad78163dd136ee500c740552e28ccc7df0ac
Wataru Maeda <w.maeda@neogenia.co.jp>
2018-06-13 21:21:01 +0900

    feat: v2 APIのブラックボックステストを書いた

--- backend-svc/docker/resources/docker-entrypoint.sh
+++ backend-svc/docker/resources/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 /etc/init.d/ssh start
 
 if [ -n "$TEST" ]; then
-  exec bash -c 'ROUTE=$TEST ruby /mnt/blackbox_test/test_v1.rb'
+  exec bash -c 'for i in /mnt/blackbox_test/*; do ROUTE=$TEST ruby $i; done'
 fi
 
 bash /mnt/xml_rpc_server/start_server.sh $RPC_SERVER_PORT
