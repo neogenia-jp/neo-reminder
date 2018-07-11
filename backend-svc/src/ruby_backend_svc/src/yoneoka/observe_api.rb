669c44c4a90be98cc77bb0080913712cb7125b13
t.yoneoka <t.yoneoka@neogenia.co.jp>
2018-07-11 22:29:55 +0900

    wip3

--- backend-svc/src/ruby_backend_svc/src/yoneoka/observe_api.rb
+++ backend-svc/src/ruby_backend_svc/src/yoneoka/observe_api.rb
@@ -9,31 +9,60 @@ module Yoneoka
     def run(json_data)
 
       notifications = []
-      @data_accessor.all_read.each do |item|
-        next unless (is_notify_target(item, json_data['options']))
-        notifications.push({subject: item['title'], body: item['memo']})
+      @data_accessor.all_read.each do |data|
+        next unless (is_notify_target(data, json_data['options']))
+        @data_accessor.update({'notify':""}, data['id'])
+        notifications.push({subject: data['title'], body: data['memo']})
       end
       {notifications: notifications}
-
     end
 
     private
-    def is_notify_target(item, options)
+
+    def is_notify_target(data, options)
       # finished_at 完了なら通知しない
-      return false if (!item['finished_at'].nil?)
+      return false if (data.has_key?('finished_at'))
+      # 通知時間未設定(＝詳細編集がまだ)なら通知しない
+      return false if (!data.has_key?('notify_datetime'))
 
-      # notify_datetime 現在時刻からxx秒以内が通知対象(とりあえず60秒)
+      # 通知時間によるチェック
       current = Time.parse(options['current_time'])
-      notify_datetime = Time.parse(item['notify_datetime'])
-      return false if (current > notify_datetime) # 通知時間が過ぎているものは対象外
-      return false if (notify_datetime - current > 60)
+      notify_datetime = Time.parse(data['notify_datetime']) # 本来の通知時間
+      if (data.has_key?('next_notify_datetime'))
+        # スヌーズあり
+        next_notify_datetime = Time.parse(data['next_notify_datetime']) # スヌーズの再通知時間
+        return false if !is_target_by_time(current, next_notify_datetime)
+      else
+        # スヌーズなし
+        return false if !is_target_by_time(current, notify_datetime)
+      end
 
-      # lat long （今後の拡張用）
-
-      return true
+      # 位置情報によるチェック
+      # 位置情報チェックをするか否か
+      is_geo = options.has_key?('lat') && options.has_key?('long') &&
+          data.has_key?('lat') && data.has_key?('long') &&
+          data.has_key?('direction') && data.has_key?('radius')
+      if (!is_geo)
+        # 位置情報チェックをしないならtrue
+        return true
+      end
+      distance = calc_distance(options['lat'], options['long'], data['lat'], data['long'])
+      is_inside = distance > data['radius'].to_i
+      if (data['direction'] == 'in')
+        is_inside
+      else
+        !is_inside
+      end
     end
 
-    def distance(lat1, lng1, lat2, lng2)
+    def is_target_by_time(current_datetime, notify_datetime)
+      return false if (current_datetime < notify_datetime) # 通知時間前なら対象外
+      return false if (current_datetime - notify_datetime > 60)
+      # 現在時刻から60秒以内
+      true
+    end
+
+    def calc_distance(lat1, lng1, lat2, lng2)
       # cf. http://techblog.kyamanak.com/entry/2017/07/09/164052
       # ラジアン単位に変換
       x1 = lat1.to_f * Math::PI / 180
--- backend-svc/src/ruby_backend_svc/src/yoneoka/snooze_api.rb
+++ backend-svc/src/ruby_backend_svc/src/yoneoka/snooze_api.rb
@@ -3,23 +3,22 @@ require_relative './base_api'
 module Yoneoka
   class SnoozeApi < BaseApi
 
-    # TODO とりあえずファイルだけ作った
-
     # 実行
     # @param json_data [Hash] 送られてきたJSONデータ
     # @return [Hash]
     def run(json_data)
       id = json_data['options']['id']
-      minutes = json_data['options']['minutes']
+      minutes = json_data['options']['minutes'].to_i
 
-      current = Time.parse(options['current_time'])
-      notify_datetime = current + minutes * 60
+      current_datetime = Time.parse(options['current_time'])
+      next_notify_datetime = current_datetime + minutes * 60
 
-      data = {'notify_datetime':notify_datetime}
+      data = {'next_notify_datetime':next_notify_datetime}
       @data_accessor.update(data, id)
       {created_at: @data_accessor.read(edit_id)['created_at']}
 
-      data['current_time'] = current
+      data['current_time'] = current_datetime
+      data #returnするのにもう一度書くのがなんかダサい…
     end
 
   end
